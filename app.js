d7b134305dad69551fb96fa6410a2aae2bae8dad
suresh <suresh.avlm@gmail.com>
2018-01-27 10:22:22 +0530

    Proc file added

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.get('/', routes.home);
 app.get('/:city', routes.city);
 
-var port = process.env.PORT | 8080;
+var port = process.env.PORT || 8080;
 
 app.listen(port, function() {
 	console.log("Server running on port: ", port);
